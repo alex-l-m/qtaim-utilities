4a21c92bac62b411067e271fd25327b490fff455
Alexander Davis <alexlovesmolecules@gmail.com>
2020-10-14 20:01:28 -0700

    Seeems to correctly label lines except for interatomic surfaces

--- parse_sum.py
+++ parse_sum.py
@@ -1,4 +1,5 @@
 import re
+import sys
 
 def parse_variables(lines):
     variable_description_dict = dict()
@@ -33,3 +34,35 @@ def parse_atomic_properties(header, rows, variable_description_dict):
                 atom_names = [i[1] for i in sorted(atom_table, key=lambda x: x[0])]
                 property_table.append(atom_names + [variable, match.group(0)])
     return property_table
+
+state = "other"
+
+for line in open(sys.argv[1]):
+    print(line, end="")
+    if re.search(": *$", line) is not None and \
+            (state == "other" or state == "variable_description_line" or \
+            state == "variable_description_interruption" or state == "possible_title"):
+        state = "possible_title"
+    elif re.match("^-+ *$", line) is not None:
+        if state == "possible_title":
+            state = "dashes_after_title"
+        elif state == "variable_description_line" or state == "variable_description_interruption":
+            state = "dashes_after_variable_descriptions"
+        elif state == "header":
+            state = "dashes_after_header"
+        elif state == "data_row":
+            state = "dashes_after_data"
+    elif re.search("=", line) is not None and \
+            (state == "dashes_after_title" or state == "variable_description_line" or \
+            state == "variable_description_interruption" or state == "possible_title"):
+        state = "variable_description_line"
+    elif re.match("^Atom A", line) is not None:
+        state = "header"
+    elif state == "variable_description_line" or state == "variable_description_interruption":
+        state = "variable_description_interruption"
+    elif re.search(r"\w+", line) is not None and \
+            (state == "dashes_after_header" or state == "data_row"):
+        state = "data_row"
+    else:
+        state = "other"
+    print(state, end="\n\n")
