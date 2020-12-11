18f7a74feb6e6e9c6839483ababc4d17b495afbf
Alexander Davis <alexlovesmolecules@gmail.com>
2020-12-10 16:38:13 -0800

    Added query string as molecule name when downloading

--- download_mol.py
+++ download_mol.py
@@ -1,4 +1,3 @@
-import functools
 from chembl_webresource_client.new_client import new_client
 from rdkit import Chem
 from rdkit.Chem import rdDistGeom
@@ -8,4 +7,5 @@ def name2mol(name):
     partial_molecule = Chem.MolFromSmiles(smiles)
     full_molecule = Chem.AddHs(partial_molecule)
     Chem.rdDistGeom.EmbedMolecule(full_molecule)
+    full_molecule.SetProp("_Name", name)
     return full_molecule
