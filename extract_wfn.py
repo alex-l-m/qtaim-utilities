1a4eb8692a57788f11289a3a5266c204fe525907
Alexander Davis <alexlovesmolecules@gmail.com>
2023-10-25 23:30:49 -0400

    Can specify functional in extract_wfn.py for AIMAll

--- extract_wfn.py
+++ extract_wfn.py
@@ -1,5 +1,14 @@
 import sys
 
+# Check for a "--functional" argument
+if len(sys.argv) > 2:
+    if sys.argv[2] == "--functional":
+        functional = sys.argv[3]
+    else:
+        raise ValueError("Unknown argument: " + sys.argv[2])
+else:
+    functional = None
+
 start_line = "----- TOP OF INPUT FILE FOR BADER'S AIMPAC PROGRAM -----\n"
 end_line = "----- END OF INPUT FILE FOR BADER'S AIMPAC PROGRAM -----\n"
 
@@ -8,6 +17,9 @@ for line in open(sys.argv[1]):
     if line == end_line:
         break
     if in_section:
-        print(line, end="")
+        if functional is None:
+            print(line, end="")
+        else:
+            print(line.replace("NUCLEI", f"NUCLEI {functional}"), end="")
     if line == start_line:
         in_section = True
